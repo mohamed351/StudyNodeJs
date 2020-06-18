58c8300cbee71c975ac4f89d4dcf89e373071bbd
Mohamed Beshri Amer <mohamed.perry351@gmail.com>
2020-06-17 22:07:21 -0700

    Express Folder for Studing Express

--- 2-Development Enviroment/app.js
+++ 2-Development Enviroment/app.js
@@ -3,8 +3,15 @@ const server = testingDevelopment.createServer((request, response) => {
     if (request.url == "/") {
         response.setHeader("Content-Type", "text/html");
         response.write("<h1>Testing Development Enviroment</h1>");
+        response.end();
+    }
+    else
+    {
+        response.statusCode = 404;
+        response.write("Not found :(");
+        response.end();
     }
 
 });
-console.log("Start Listening port 3000");
+console.log("Start Listening port 3000..");
 server.listen(3000);
